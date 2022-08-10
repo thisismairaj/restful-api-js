cfba4f056a1142c2491379279bbfd9e82fb53876
Mairaj Pirzada <thisismairaj@gmail.com>
2022-08-10 17:34:58 +0500

    something

---
+++ models/subscriber.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subscribedToChannel: {
+        type: String,
+        required: true
+    },
+    subscribeDate: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+});
